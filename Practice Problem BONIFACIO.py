8c547f6e70b6d2a8fde9655838a6705b6b9f7bfc
RedjBonifacio <qrgfbonifacio@tip.edu.ph>
2024-08-30 16:48:27 +0800

    Add files via upload

---
+++ Practice Problem BONIFACIO.py
@@ -0,0 +1,25 @@
+def score_given():
+   for score in inputScores:
+       if score % 2 == 0:
+           even_score.append(score)
+       else:
+           pass
+             
+            
+inputScores = [26, 49, 98, 87, 62, 75]
+even_score = []
+
+score_given()         
+print(even_score)
+print(sum(even_score))
+           
+ 
+           
+           
+    
+           
+          
+
+            
+            
+            
